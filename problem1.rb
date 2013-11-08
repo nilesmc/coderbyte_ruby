ae57de03e6cded29ceb4bf1e1b018f40aa1fb69f
garbagemanonfire <nilesvm@gmail.com>
2013-11-08 13:55:15 -0800

    working copy

---
+++ problem1.rb
@@ -0,0 +1,10 @@
+def FirstReverse(str)
+
+  str.reverse!
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstReverse(STDIN.gets)  
