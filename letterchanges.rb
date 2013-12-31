7363186dce35a3255525b96cb0922d8eae07f596
garbagemanonfire <nilesvm@gmail.com>
2013-12-31 14:56:25 -0800

    up through the 6th ruby problem with correction

--- letterchanges.rb
+++ letterchanges.rb
@@ -1,11 +1,17 @@
-def LetterCapitalize(str)
-
-  arr = str.split(" ")
+def LetterChanges(string)
+  alphabet = ("a".."z").to_a
+  output_str = ""
+  letter_holder = ""
   
-  arr.map!{|x| x.capitalize}
+  string.chars.to_a.each do |letter|
+    letter_holder = letter
+    alphabet.each_with_index do |character, index|
+      if character == letter
+        letter_holder = alphabet[index+1]    
+      end
+    end
+    output_str = output_str + "#{letter_holder}"
+  end
   
-  str = arr.join(" ")
-
-  return str 
-         
+  return output_str.tr!("aeiou", "AEIOU") 
 end
